4815b0b71cb60ef2947b62b2d105935f1992793f
umesh.kumar <umesh.kumar@yatra.com>
2025-03-19 13:49:07 +0530

    fix:contact us

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -52,7 +52,7 @@ export default function Footer() {
           onClick={() => setIsOpen(true)}
           className="mt-4 md:mt-0 bg-green-600 hover:bg-green-700 text-white px-5 py-2 rounded-lg shadow-md transition duration-300 cursor-pointer"
         >
-          Share Feedback
+          Contact Us
         </button>
         <p className="text-sm text-gray-400 mt-2">
           &copy; {new Date().getFullYear()} Fitnum. All rights reserved.
@@ -62,7 +62,7 @@ export default function Footer() {
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
           <div className="bg-white p-6 rounded-lg shadow-xl w-96">
             <h2 className="text-2xl font-bold text-gray-800 mb-4">
-              Share Your Feedback
+              Do You want to Change Yourself?
             </h2>
             <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
               <input
